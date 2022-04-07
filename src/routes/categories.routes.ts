93b88665febc67fb2eff393d08a0ed0ec96cc85c
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-04-07 17:51:15 -0300

    Implemented LSP and DIP using SOLID

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,6 +12,7 @@ categoriesRoutes.post("/", (request, response) => {
   const createCategoryService = new CreateCategoryService(categoriesRepository);
 
   createCategoryService.execute({ name, description });
+
   return response.status(201).send();
 });
 
