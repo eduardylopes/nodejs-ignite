4fec82531be5e40d24fb23f5092225c79fc157e9
Eduardy Lopes de Morais <eduardylopes@gmail.com>
2022-05-11 21:16:15 -0300

    test create category supertest

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -9,7 +9,7 @@ import createConnection from "@shared/infra/typeorm/index";
 let connection: Connection;
 
 describe("Create Category Controller", () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
 
@@ -22,16 +22,22 @@ describe("Create Category Controller", () => {
     );
   });
 
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it("should be able to create a new category", async () => {
     const responseToken = await request(app)
       .post("/sessions")
       .send({ email: "admin@rentx.com.br", password: "admin" });
 
-    console.log(responseToken.body);
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
-      .send({ name: "Category Supertest", description: "Category Supertest" });
+      .send({ name: "Category Supertest", description: "Category Supertest" })
+      .set({ Authorization: `Bearer ${token}` });
 
     expect(response.status).toBe(201);
   });
